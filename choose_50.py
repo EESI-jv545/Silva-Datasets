0389ac001ef6e3e28546cac61927cc0bdbe27858
EESI-jv545 <115170707+EESI-jv545@users.noreply.github.com>
2023-03-23 16:01:58 -0400

    Update choose_50.py

--- choose_50.py
+++ choose_50.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 
 ranks=['phylum', 'class', 'order', 'family', 'genus']
-prefix='Prokaryotes_fixed_'
+prefix=''
 output='results'
 
 labels_divider=0.5   #change this variable to change the percentage of labels to consider for testing
@@ -12,7 +12,7 @@ for r in ranks:
     csv=pd.read_csv(output+'/seqs_per_rank/'+prefix+'seqs_for_each_'+r+'.csv')
 
     labels=list(csv.loc[:,(r+' label')])
-    good_labels=[csv.loc[x,(r+' label')] for x in range(0,len(csv.loc[:,(r+' label')])) if int(csv.loc[x, 'total sequences']) >= 2]
+    good_labels=[csv.loc[x,(r+' label')] for x in range(0,len(csv.loc[:,(r+' label')])) if int(csv.loc[x, 'total sequences']) >= 2]   #choose 50% of the labels with 2 or more sequences; change this variable as needed
     # pdb.set_trace()
     chosen_labels=random.sample(good_labels, int(labels_divider*len(good_labels)))
 
