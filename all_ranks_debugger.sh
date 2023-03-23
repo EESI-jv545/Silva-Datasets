f370ea84d3e4156984ff9e2db19e4a9390250d30
EESI-jv545 <115170707+EESI-jv545@users.noreply.github.com>
2023-03-23 16:02:57 -0400

    Update all_ranks_debugger.sh

--- all_ranks_debugger.sh
+++ all_ranks_debugger.sh
@@ -4,13 +4,15 @@
 percentage_prefix="10"
 labels_divider_perfix="50"
 labels_seqs_divider_prefix="50"
+directory=results/full_datasets_fixed
+dataset_name=fixed_
 
 for rank in phylum class order family genus species
 do
-python dataset_debugger.py $rank
+python dataset_debugger.py $rank $dataset_name
 echo REALITY FOR $percentage_prefix%:
-grep '>' results/full_datasets_Prokaryotes/training_${percentage_prefix}-percent-${rank}.txt | wc -l
+grep '>' $directory/training_${percentage_prefix}-percent-${rank}.txt | wc -l
 echo REALITY FOR $labels_divider_perfix%
-grep '>' results/full_datasets_Prokaryotes/training_${labels_divider_perfix}-${labels_seqs_divider_prefix}-${rank}.txt | wc -l
+grep '>' $directory/training_${labels_divider_perfix}-${labels_seqs_divider_prefix}-${rank}.txt | wc -l
 echo ----------------
 done
